92d4d8092dac4e726f9614e83c1ff519e11a7630
Scorp3y <artem.turovetc@gmail.com>
2025-06-20 02:58:03 +0300

    1.0.7.0.1a v

---
+++ Assets/Project/Scripts/TutorialController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    public GameObject tutorialChoiceUI; 
+    public GameObject tutorialUI;      
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("hasCompletedTutorial", 0) == 1)
+        {
+            tutorialChoiceUI.SetActive(false);
+            tutorialUI.SetActive(false);
+        }
+        else
+        {
+            tutorialChoiceUI.SetActive(true);
+        }
+    }
+
+    public void OnChooseTutorialYes()
+    {
+        tutorialChoiceUI.SetActive(false);
+        tutorialUI.SetActive(true);
+    }
+
+    public void OnChooseTutorialNo()
+    {
+        PlayerPrefs.SetInt("hasCompletedTutorial", 1);
+        PlayerPrefs.Save();
+
+        tutorialChoiceUI.SetActive(false);
+        tutorialUI.SetActive(false);
+    }
+
+    [ContextMenu("Сбросить обучение")]
+    public void ResetTutorial()
+    {
+        PlayerPrefs.DeleteKey("hasCompletedTutorial");
+        PlayerPrefs.Save();
+        Debug.Log("Обучение сброшено (hasCompletedTutorial удалён).");
+    }
+}
